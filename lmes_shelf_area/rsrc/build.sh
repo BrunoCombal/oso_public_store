e01b6da4db0e1b8042542e617d46e37755da6e03
GeowowRoot <root@geowow.iode.org>
2015-04-03 15:54:28 +0200

    update lmes_shelf_area

---
+++ lmes_shelf_area/rsrc/build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Author: Bruno Combal
+# Date: August 2014
+
+shopt -s nullglob
+shopt -s dotglob
+
+path=/data/public_store/lmes_shelf_area
+rsrcDir=${path}/rsrc
+outdir=${path}/
+tmpdir=${path}/tmp
+mkdir -p ${tmpdir}
+
+# some operations should be done manually, including deleting the tmp directory and update information files
+chktmp=(${tmpdir}/*)
+if [ ${#chktmp[*]} -ne 0 ]; then echo "please clean tmp directory and update information files"; exit 1; fi
+
+# copy shapefile
+for iext in cpg csv dbf prj qpj shp shx
+do
+    cp ${path}/lmes_shelf_area.${iext} ${tmpdir}
+done
+
+# copy information files
+# rule: all files last change must be from the same day. Else, require the user to update them
+memoDay='None'
+for ii in readme.txt version.txt productDescription_lmes_shelf_area.txt lmes_shelf_area_xml_iso_19139.xml
+do
+    if [ ! -e "${ii}" ]; then
+	echo "information file ${ii} does not exist. Exit."
+	exit 3
+    fi
+
+    thisDay=$(stat ${rsrcDir}/${ii} |grep Change | cut -d ' ' -f 2)
+    if [ "${memoDay}" != 'None' ]; then
+	if [ "${thisDay}" != "${memoDay}" ]; then
+	    echo "Found information files with different last update day."
+	    echo "Please review information files and run again this building script."
+	    exit 2
+	fi
+    fi
+    cp ${rsrcDir}/${ii} ${tmpdir}
+done
+
+version=$(cat ${tmpdir}/version.txt | grep -i version | head -n 1 | cut -d ':' -f 2 | tr -d '[:space:]')
+
+
+if [ -n "${version}" ]; then
+    archiveName=lmes_shelf_area_${version}.zip
+else
+    archiveName=lmes_shelf_area.zip
+fi
+
+# zip all files
+rm -f ${archiveName}
+
+find ${tmpdir} -name "*" | zip -@ -j ${outdir}/${archiveName}
+
+# end of script
---
+++ lmes_shelf_area/rsrc/readme.txt
@@ -0,0 +1,22 @@
+Transboundary Water Assessment Program (GEF funded project)
+Marine component: http://onesharedocean.org
+TWAP: www.geftwap.org
+
+Product: LMEs Shelf Area
+
+Package content:
++ readme.txt: this file
++ version.txt: current version and history of changes
++ productDescription_lmes_shelf_area.txt: scientific decription
++ dataset: shapefile lme_shelf_area.shp, see below
+
+datasets:
+CSV (comma separated values): lme_shelf_area.csv, see fields description below.
+
+Shapefile "lme_governance_analysis_indicators.shp" has the following fields:
++ LME_NAME: LME name
++ LME_NUMBER: LME number, from 1 to 66
++ shelf_area: total shelf area of the LME, in square kilometer
+
+
+--- end of file ---
