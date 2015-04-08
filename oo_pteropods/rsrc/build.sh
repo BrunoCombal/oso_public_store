0f86969e9f705fbb5b9eca6d261d1063be4dd9f3
GeowowRoot <root@geowow.iode.org>
2015-04-08 17:26:28 +0200

    delete ancient zip before rebuilding. Adds shapefiles

--- oo_pteropods/rsrc/build.sh
+++ oo_pteropods/rsrc/build.sh
@@ -9,6 +9,7 @@ shopt -s dotglob
 path=/data/public_store/oo_pteropods/
 rsrcDir=${path}/rsrc
 outdir=${path}/
+shpdir=${path}/shp
 tmpdir=${path}/tmp
 mkdir -p ${tmpdir}
 
@@ -26,47 +27,49 @@ for ispecies in ${species[@]}
 do
     for ircp in ${rcps[@]}
     do
-	for idecade in ${decades[@]}
-	do
+        for idecade in ${decades[@]}
+        do
     # copy information files
     # rule: all files last change must be from the same day. Else, require the user to update them
-	    memoDay='None'
-	    for ii in readme.txt version.txt oo_${ispecies}_${idecade}_${ircp}_xml_iso19139.xml productDescription_oo_pteropods.txt 
-	    do
-		if [ ! -e "${ii}" ]; then
-		    echo "information file ${ii} does not exist. Exit."
-		    exit 3
-		fi
-		
-		thisDay=$(stat ${rsrcDir}/${ii} |grep Change | cut -d ' ' -f 2)
-		if [ "${memoDay}" != 'None' ]; then
-		    if [ "${thisDay}" != "${memoDay}" ]; then
-			echo "Found information files with different last update day."
-			echo "Please review information files and run again this building script."
-			exit 2
-		    fi
-		fi
-		cp ${rsrcDir}/${ii} ${tmpdir}
-	    done
-	    
-	    cp ${path}/${ispecies}_arag_temp_${idecade}_${ircp}.tif ${tmpdir}
+            memoDay='None'
+            for ii in readme.txt version.txt oo_${ispecies}_${idecade}_${ircp}_xml_iso19139.xml productDescription_oo_pteropods.txt
+            do
+                if [ ! -e "${ii}" ]; then
+                    echo "information file ${ii} does not exist. Exit."
+                    exit 3
+                fi
 
-	    version=$(cat ${tmpdir}/version.txt | grep -i version | head -n 1 | cut -d ':' -f 2 | tr -d '[:space:]')
-	    
-	    if [ -n "${version}" ]; then
-		archiveName=oo_${ispecies}_${idecade}_${ircp}_${version}.zip
-	    else
-		archiveName=oo_${ispecies}_${idecade}_${ircp}.zip
-	    fi
-	    
+                thisDay=$(stat ${rsrcDir}/${ii} |grep Change | cut -d ' ' -f 2)
+                if [ "${memoDay}" != 'None' ]; then
+                    if [ "${thisDay}" != "${memoDay}" ]; then
+                        echo "Found information files with different last update day."
+                        echo "Please review information files and run again this building script."
+                        exit 2
+                    fi
+                fi
+                cp ${rsrcDir}/${ii} ${tmpdir}
+            done
+
+            cp ${path}/${ispecies}_arag_temp_${idecade}_${ircp}.tif ${tmpdir}
+
+	    cp ${shpdir}/${ispecies}_*_${idecade}_${ircp}.??? ${tmpdir}
+
+            version=$(cat ${tmpdir}/version.txt | grep -i version | head -n 1 | cut -d ':' -f 2 | tr -d '[:space:]')
+
+            if [ -n "${version}" ]; then
+                archiveName=oo_${ispecies}_${idecade}_${ircp}_${version}.zip
+            else
+                archiveName=oo_${ispecies}_${idecade}_${ircp}.zip
+            fi
+
             # zip all files
-	    rm -f ${archiveName}
-	    find ${tmpdir} -name "*" | zip -@ -j ${outdir}/${archiveName}
+            rm -f ${outdir}/${archiveName}
+            find ${tmpdir} -name "*" | zip -@ -j ${outdir}/${archiveName}
 
-	    # rm data in tmp
-	    rm -f ${tmpdir}/*
-	    
-	done
+            # rm data in tmp
+            rm -f ${tmpdir}/*
+
+        done
     done
 done
 
